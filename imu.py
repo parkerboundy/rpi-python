1f4c6441b79dcb99bdcac0c59a975107d692a467
Parker Boundy <parkerboundy@gmail.com>
2013-05-26 01:00:13 -0400

    read mag and accel

--- imu.py
+++ imu.py
@@ -1,7 +1,8 @@
 import smbus
 
 class IMU: 
-
+	
+	# register addresses
 	MAG_ADDRESS = (0x3C >> 1)
 	ACC_ADDRESS = (0x32 >> 1)
 	ACC_ADDRESS_SA0_A_LOW = (0x30 >> 1)
@@ -10,13 +11,13 @@ class IMU:
 	LSM303_CTRL_REG4_A = 0x23
 	LSM303_MR_REG_M = 0x02
 	LSM303_OUT_X_L_A = 0x28
+	LSM303_OUT_X_H_M = 0x03
 
 	def __init__(self):
 		self.bus = smbus.SMBus(1)
 		# note: don't need to pass self directly since it is implicitly passed by python
 		self.writeAccReg(self.LSM303_CTRL_REG1_A, 0x27)
 		self.writeMagReg(self.LSM303_MR_REG_M, 0x00)
-		print self.readAccReg(self.LSM303_OUT_X_L_A | 0x80, 6) 
 
 	def writeAccReg(self, register, value):
 		self.bus.write_byte_data(self.ACC_ADDRESS, register, value)
@@ -27,17 +28,20 @@ class IMU:
 	def writeMagReg(self, register, value):
 		self.bus.write_byte_data(self.MAG_ADDRESS, register, value)
 
-	def readMagReg(self, register):
-		return 0
+	def readMagReg(self, register, length):
+		return self.bus.read_i2c_block_data(self.MAG_ADDRESS, register, length)
 
 	def readAcc(self):
-		return 0
+		data = self.readAccReg(self.LSM303_OUT_X_L_A | 0x80, 6)
+		return [self.__convert_accel(data, 0), self.__convert_accel(data, 2), self.__convert_accel(data, 4)]
 
 	def readMag(self):
-		return 0
+		data = self.readMagReg(self.LSM303_OUT_X_H_M, 6)
+		return [self.__convert_mag(data, 0), self.__convert_mag(data, 2), self.__convert_mag(data, 4)]
 
 	def read(self):
-		return 0
+		self.readAcc()
+		self.readMag()
 
 	def heading(self):
 		return 0
@@ -50,3 +54,12 @@ class IMU:
 
 	def vector_normalize(self):
 		return 0
+	
+	def __convert_accel(self, list, index):
+		n = list[index] | (list[index+1] << 8)
+		if n > 32767: n -= 65536
+		return n >> 4
+	
+	def __convert_mag(self, list, index):
+		n = (list[index] << 8) | list[index+1]
+		return n if n < 32768 else n - 65536
