20dc22b89497b8afcf26b45f48783fc4eeff9255
Parker Boundy <parkerboundy@gmail.com>
2013-07-26 15:38:50 -0400

    added conver_to_bytes to lcd.py

--- lcd.py
+++ lcd.py
@@ -7,4 +7,11 @@ class LCD:
 		self.bus = smbus.SMBus(1)
 
 	def write_data(self, data):
-		self.bus.write_i2c_block_data(self.LCD_ADDRESS, 0, data)
+		self.bus.write_i2c_block_data(self.LCD_ADDRESS, 0, self.convert_to_bytes(data))
+
+	def convert_to_bytes(self, data):
+		bytes = []
+		data = str(data)
+		for item in data:
+			bytes.append(ord(item))
+		return bytes
