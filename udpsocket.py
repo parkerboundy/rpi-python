31e6ad2d3a86744d1ed920aabf94aad74054d828
Parker Boundy <parkerboundy@gmail.com>
2013-09-16 17:30:51 -0400

    added udpsocket.py

---
+++ udpsocket.py
@@ -0,0 +1,11 @@
+import socket
+
+class UDPSocket:
+
+	def __init__(self):
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+	def send(self, data):
+		s.sendto(data, ('<broadcast>', 5050))
+
