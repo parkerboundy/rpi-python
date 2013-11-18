754d9ab2d1d2959b00a4634571e3ded3395385ea
parkerboundy <parker@Parkers-MacBook-Pro.local>
2013-11-18 18:09:55 +0800

    udpsocket.py runs on background thread

--- box/udpsocket.py
+++ box/udpsocket.py
@@ -1,11 +1,22 @@
+import threading
+import Queue
 import socket
 
-class UDPSocket:
+class UDPSocket(threading.Thread):
 
-	def __init__(self):
+	def __init__(self, queue):
+		threading.Thread.__init__(self)
+		self.queue = queue
+		self.daemon = True
+		
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-	def send(self, data):
+	def _send(self, data):
 		self.s.sendto(data, ('<broadcast>', 5050))
 
+	def run(self):
+		while True:
+			message = self.queue.get()
+			self._send(message)
+			self.queue.task_done()
