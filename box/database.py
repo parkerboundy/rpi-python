09a5ee69bfe30a5aa0c7c808ef594d007f6c430a
parkerboundy <parker@Parkers-MacBook-Pro.local>
2013-11-18 18:10:04 +0800

    database.py runs on background thread

--- box/database.py
+++ box/database.py
@@ -1,12 +1,18 @@
+import threading
+import Queue
 import sqlite3
 
-class Database:
+class Database(threading.Thread):
 
-	def __init__(self):
-		self.con = sqlite3.connect('box.db')
-		self.cur = self.con.cursor()
+	def __init__(self, queue):
+		threading.Thread.__init__(self)
+		self.queue = queue
+		self.daemon = True
 
-		self._check_schema()
+		#self.con = sqlite3.connect('box.db')
+		#self.cur = self.con.cursor()
+
+		#self._check_schema()
 
 	def _check_schema(self):
 		self.cur.execute("SELECT 1 FROM sqlite_master WHERE type='table' AND name='points';")
@@ -21,4 +27,13 @@ class Database:
 		self.cur.executescript(sql)
 
 	def insert(self, datapoint):
-		self.cur.execute("INSERT INTO points(speed) VALUES (?)", (123,))
+		con = sqlite3.connect('box.db')
+		cur = con.cursor()
+		cur.execute("INSERT INTO points(speed) VALUES (?)", (123,))
+		con.close()
+
+	def run(self):
+		while True:
+			data = self.queue.get()
+			self.insert(data)
+			self.queue.task_done()
