dd50ed546e14fc36a0a574381bfffa2cb15ba529
parkerboundy <parker@Parkers-MacBook-Pro.local>
2013-11-18 22:44:24 +0800

    added SocketServer class

--- box/udpsocket.py
+++ box/udpsocket.py
@@ -3,7 +3,7 @@ import Queue
 import socket
 import logging
 
-class UDPSocket(threading.Thread):
+class SocketClient(threading.Thread):
 
 	def __init__(self, queue):
 		self.log = logging.getLogger(__name__)
@@ -25,3 +25,17 @@ class UDPSocket(threading.Thread):
 			self.log.info('Received message from queue "%s"', message)
 			self.send(message)
 			self.queue.task_done()
+
+class SocketServer(threading.Thread):
+	
+		def __init__(self):
+			threading.Thread.__init__(self)
+			self.daemon = True		
+
+			self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			self.s.bind(('', 5050))
+
+		def run(self):
+			while True:
+				data = self.s.recv(1024)
+				print data
