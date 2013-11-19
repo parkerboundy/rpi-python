58c306d8244fb69f0a8e8cd549d649885b498875
parkerboundy <parker@Parkers-MacBook-Pro.local>
2013-11-19 23:30:28 +0800

    created util module

---
+++ box/util.py
@@ -0,0 +1,13 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Sailing Data Collection Server')
+parser.add_argument('-p','--port_number', type=int, metavar='Port', help='UDP port number', default=5050)
+parser.add_argument('-l', '--log_level', type=str, choices=['debug', 'info', 'warning', 'error', 'critical'], help='Log level to use', default='debug')
+
+
+settings = {}
+settings['port'] = 5050;
+settings['log_file'] = 'logs/box.log'
+settings['log_level'] = 'DEBUG'
+settings['log_format'] = '%(asctime)s:%(levelname)s:%(module)s:%(message)s'
+settings['database_file'] = 'db/box.db'
