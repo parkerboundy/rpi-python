7a4c66f0313ab5a3fc15f68f77d1ec47b9d45520
parkerboundy <parker@Parkers-MacBook-Pro.local>
2013-11-20 14:41:35 +0800

    moved JS to custom.js

---
+++ analyze/static/js/custom.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  $('.list-group a').click(function() {
+  	switch(this.id){
+  		case "all":
+  			$(':hidden').show();
+  			break;
+
+  		case "race":
+  			$('.race').show();
+  			$('.practice').hide();
+  			break;
+
+		case "practice":
+			$('.race').hide();
+			$('.practice').show();
+			break;
+    }
+	 $('.active').removeClass('active');
+	 $(this).addClass('active');
+  });
+
+
+  $( "#slider" ).slider({
+    disabled: true,
+    range: true,
+    min: 0,
+    max: 100,
+    values: [ 0, 100],
+    slide: function( event, ui ) {
+      //DO SOMETHING HERE
+      //$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    }
+  });
+
+  $(':input[data-loading-text]').click(function () {
+    $(this).button('loading')  
+  });
+
+  $("#add-form #date").change(function() {
+    if($(this).val() != "") {
+      $("#add-form :input ").attr("disabled", false);
+      $("#slider").slider("option", "disabled", false);
+    }
+    else {
+      $("#add-form :input:not(#date)").attr("disabled", true);
+      $("#slider").slider("option", "disabled", true);
+    }
+  });
+
+});
